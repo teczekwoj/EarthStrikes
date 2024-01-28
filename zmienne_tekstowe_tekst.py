3f7484312792e5713e23c098c5471d48e86fb4fe
Wojtek <wlkr2000@gmail.com>
2024-01-28 13:28:18 +0100

    add text

--- zmienne_tekstowe_tekst.py
+++ zmienne_tekstowe_tekst.py
@@ -1,42 +1,57 @@
-text_chondryty = """**Chondryty** to grupa meteorytów kamiennych, które zawierają chondrule, czyli małe, kuliste ziarna utworzone we wczesnym okresie formowania się Układu Słonecznego."""
+text_chondryty = """Mając ponad 4,5 miliarda lat, **chondryty** są jednymi z najbardziej prymitywnych i dziewiczych skał w Układzie Słonecznym i nigdy nie zostały stopione.
+Chondryty mają charakterystyczny wygląd, wykonane z kropelek minerałów krzemianowych zmieszanych z drobnymi ziarnami siarczków i metalu żelazowo-niklowego. Ich milimetrowe granulki nadają chondrytom ich nazwę, od greckiego "chondres" oznaczającego ziarna piasku.
+Istnieje wiele odmian chondrytu, z różnicami w mineralogii związanymi z typem asteroidy, z której pochodził meteoryt.
+Chondryty to materiał, z którego uformował się Układ Słoneczny. Uległy one niewielkim zmianom w porównaniu ze skałami z większych planet, które zostały poddane aktywności geologicznej. Chondryty mogą nam wiele powiedzieć o tym, jak powstał Układ Słoneczny. 82% procent spadających meteorytów to chondryty."""
 
-text_chondryty_zwyczajne = """**Chondryty zwyczajne** to najczęstszy typ meteorytów kamiennych, a są one dodatkowo podzielone na grupy H, L i LL w zależności od zawartości metalu."""
+text_chondryty_zwyczajne = """**Chondryty zwyczajne** - grupa meteorytów kamiennych należąca do chondrytów. Stanowią ok. 80% wszystkich znajdowanych meteorytów. Chondryty zwyczajne dzielą się na trzy grupy: chondryty oliwinowo-bronzytowe (H), chondryty oliwinowo-hiperstenowe (L), chondryty oliwinowo-pigeonitowe (LL, nazywane też amfoterytami). Chondryty zwyczajne zawierają w swym składzie: piroksen rombowy 20-30%, plagioklaz 5-10%, troilit ok. 5%. Poza tym posiadają również oliwin, kamacyt i taenit w proporcjach zależnych od typu chondrytu. Nie zawierają minerałów z grupą wodorotlenową OH."""
 
-text_chondryty_enstatytowe = """**Chondryty enstatytowe** to rzadki rodzaj chondrytów, charakteryzujący się wysoką zawartością enstatytu."""
+text_chondryty_enstatytowe = """**Chondryty enstatytowe** - grupa meteorytów kamiennych. Jest to najrzadziej spotykany typ meteorytów kamiennych oznaczonych symbolem E. W chondrytach enstatytowych występuje żelazo w postaci metalicznej (kamacyt) i w połączeniu z siarką tworząc troilit (FeS, siarczek żelaza). W meteorytach tych występuje: enstatyt, minerał krzemianowy z magnezowej grupy piroksenów MgSiO3. Może on tworzyć wyraźnie zarysowane struktury chondr enstatytowych. Poza tym w meteorytach tych występują: kwarc, trydymit, krystobalit. Brak w nim natomiast oliwinu oraz krzemianowych minerałów zawierających żelazo.
+Pierwszym meteorytem opisanym z tej grupy, był meteoryt spadły w 1901 roku w południowo-zachodniej Finlandii. Jak dotąd największym chondrytem enstatytowym jest kanadyjski meteoryt Abee."""
 
-text_chondryty_węgliste = """**Chondryty węgliste** są bogate w związki węgla i materiały organiczne, dostarczając cennych informacji na temat wczesnego Układu Słonecznego i budulców życia."""
+text_chondryty_węgliste = """**Chondryty węgliste** - grupa meteorytów kamiennych o symbolu ogólnym C. Charakteryzują się bardzo kruchą budową i dlatego przeważnie rozpadają się w trakcie przelotu przez ziemską atmosferę. Dotyczy to głównie grupy C1 i C2. Chondryty węgliste zawierają dużo węgla, do ok. 4% oraz dużo wody, do ok. 20%. Poza tym zawierają krzemiany z grupą wodorotlenową OH i magnetyt (do 25%).
+Po raz pierwszy analizie chemicznej poddano chondryt węglisty w 1834 roku. Najbardziej znanym chondrytem węglistym jest meksykański meteoryt Allende."""
 
-text_kakangari_type = """**Typ Kakangari** to unikalna klasyfikacja meteorytów o specyficznych cechach mineralogicznych."""
+text_kakangari_type = """Trzy chondryty tworzą tak zwaną **grupę K (typu Kakangari)**: Kakangari, LEW 87232 i Lea Co. 02. Charakteryzują się dużą ilością pyłowej matrycy i składem izotopów tlenu podobnym do chondrytów węglowych, silnie zredukowanym składem mineralnym i wysoką zawartość metali (6-10% objętościowo), które najbardziej przypominają chondryty enstatytowe, oraz stężenia ogniotrwałych pierwiastków litofilowych, które najbardziej przypominają zwykłe chondryty.
+Wiele innych ich cech jest podobnych do chondrytów O, E i C."""
 
-text_rumurutiites = """**Rumurutiity** to rodzaj prymitywnych achondrytów, uważanych za pochodzące z ciała macierzystego, które doświadczyło minimalnej dyferencjacji."""
+text_rumurutiites = """**Chondryty typu Rumuruti (R)** są bardzo rzadką grupą, z tylko jednym udokumentowanym uderzeniem z prawie 900 udokumentowanych spadków chondrytów. Mają wiele właściwości wspólnych ze zwykłymi chondrytami, w tym podobne typy chondr, kilka wtrąceń ogniotrwałych, podobny skład chemiczny większości pierwiastków oraz fakt, że stosunki 17O/16O są nienormalnie wysokie w porównaniu ze skałami ziemskimi. Istnieją jednak znaczące różnice między chondrytami R a zwykłymi chondrytami: chondryty R mają znacznie więcej pylącego materiału matrycy (około 50% skały), są znacznie bardziej utlenione i zawierają niewielką ilość metalicznego Fe-Ni; a ich wzbogacenie jest wyższe o 170 niż w przypadku zwykłych chondrytów. Prawie cały metal zawarty w nich jest utleniony lub w postaci siarczków. Zawierają mniej chondr niż chondryty E i wydają się pochodzić z regolitu asteroidy."""
 
-text_chondryty_h = """**Chondryty H** to podgrupa chondrytów zwyczajnych, charakteryzująca się wyższą zawartością metalu."""
+text_chondryty_h = """**Chondryty H** to podgrupa chondrytów zwyczajnych, charakteryzująca się wyższą zawartością metalu (12 do 21% żelaza metalicznego) (zwane także chondrytami brązu) 31,4% upadków. Minerały: oliwin, piroksen, metal, plagioklaz, siarczek."""
 
-text_chondryty_l = """**Chondryty L** to kolejna podgrupa chondrytów zwyczajnych, ze średnią zawartością metalu."""
+text_chondryty_l = """**Chondryty L** to kolejna podgrupa chondrytów zwyczajnych, ze średnią zawartością metalu (5 do 10% żelaza metalicznego) (zwane także chondrytami hiperstenowymi) 34,8% upadków. Minerały: oliwin, piroksen, plagioklaz, metal, siarczek."""
 
-text_chondryty_ll = """**Chondryty LL** to trzecia podgrupa chondrytów zwyczajnych, z niską zawartością metalu."""
+text_chondryty_ll = """**Chondryty LL** to trzecia podgrupa chondrytów zwyczajnych, z niską zawartością metalu (około 2% metalicznego żelaza) (zwanych także amfoterytami). Podstawowymi minerałami są bronzyt, oliwin i mniejsze oligoklazy. 7,2% upadków."""
 
-text_achondryty = """**Achondryty** to meteoryty, które przeszły częściowe stopienie i różnicowanie się na ciałach macierzystych."""
+text_achondryty = """**Achondryt** to kamienny meteoryt, który nie zawiera chondr. Składa się z materiału podobnego do ziemskich bazaltów lub skał plutonicznych i został w mniejszym lub większym stopniu zróżnicowany i ponownie przetworzony w wyniku topnienia i rekrystalizacji na ciałach macierzystych meteorytów lub w ich obrębie. W rezultacie achondryty mają odrębną teksturę i mineralogię wskazującą na procesy magmowe.
+Achondryty stanowią około 8% wszystkich meteorytów, a większość (około dwie trzecie) z nich to meteoryty HED, prawdopodobnie pochodzące ze skorupy asteroidy 4 Westa. Inne typy obejmują marsjańskie, księżycowe i kilka typów, które prawdopodobnie pochodzą z jeszcze niezidentyfikowanych asteroid."""
 
-text_howardites = """**Howardity** to rodzaj achondrytów z grupy HED, uważanych za pochodzące z asteroidy Westa."""
+text_howardites = """**Howardity** to rodzaj achondrytów z grupy HED, uważanych za pochodzące z asteroidy Westa. Znanych jest około 200 różnych obiektów."""
 
-text_eucrites = """**Eukryty** to kolejna podgrupa achondrytów HED, składająca się głównie z bazaltowej skały."""
+text_eucrites = """**Eukryty** to achondrytyczne meteoryty kamienne, z których wiele pochodzi z powierzchni asteroidy 4 Westa i należy do klanu meteorytów HED. Są najczęstszą grupą achondrytów, w której znaleziono ponad 100 meteorytów.
+Eukryt składa się ze skał bazaltowych ze skorupy 4 Westy lub podobnego ciała macierzystego. Składają się głównie z ubogiego w wapń piroksenu, gołębi i bogatego w wapń plagioklazu (anortyt)."""
 
-text_diogenite = """**Diogenity** to achondryty z grupy HED, charakteryzujące się wysoką zawartością piroksenu."""
+text_diogenite = """**Diogenity** - uważa się, że diogenity pochodzą z głębi skorupy asteroidy 4 Westa i jako takie należą do klanu meteorytów HED. Znanych jest około 40 różnych członków.
+Diogenity składają się ze skał magmowych pochodzenia plutonowego, które krzepły wystarczająco głęboko w skorupie Westy, tworząc kryształy większe niż w eukrytach. Kryształy te to przede wszystkim ortopiroksen bogaty w magnez z niewielkimi ilościami plagioklazów i oliwinu."""
 
-text_shergottites = """**Szerogity** to grupa meteorytów marsjańskich, pochodzących z Marsa, zawierające skały wulkaniczne."""
+text_shergottites = """**Szerogity** to grupa meteorytów marsjańskich, pochodzących z Marsa, zawierające skały wulkaniczne. Mniej więcej trzy czwarte wszystkich meteorytów marsjańskich można sklasyfikować jako shergotyty. Ich nazwa pochodzi od meteorytu Shergotty, który spadł w Sherghati w Indiach w 1865 roku. Shergotyty do skał magmowych. Dzielą się na trzy główne grupy: bazaltowe, oliwinowo-firowe (takie jak Tissint znaleziona w Maroku w 2011 r i shergotyty lherzolitowe, w zależności od ich wielkości kryształów i zawartości minerałów. Można je alternatywnie zastosować na trzy lub cztery grupy w zależności od zawartości pierwiastków ziem rzadkich.Te dwa systemy wyszukiwania nie pokrywają się ze sobą, co wskazuje na powiązania między szczątkami źródeł i magmami, z których wynikają się shergotyty."""
 
-text_nakhlites = """**Nahklity** to kolejna grupa meteorytów marsjańskich, składająca się głównie z piroksenów i chlorytów."""
+text_nakhlites = """**Nahklity** to kolejna grupa meteorytów marsjańskich, składająca się głównie z piroksenów i chlorytów. Nachlity zostały nazwane na cześć pierwszego z nich, meteorytu Nakhla, który spadł w El-Nakhla w Aleksandrii w Egipcie w 1911 roku i miał szacunkową masę 10 kg.
+Nachlity to skały magmowe bogate w augit, utworzone z magmy bazaltowej w wyniku co najmniej czterech erupcji, które miały miejsce około 90 milionów lat temu. Zawierają kryształy augitu i oliwinu. Wiek ich krystalizacji, porównany z chronologią liczby kraterów w różnych regionach Marsa, sugeruje, że nakhlity utworzyły się na dużej konstrukcji wulkanicznej Tharsis, Elysium lub Syrtis Major Planum.
+Wykazano, że nakhlity zostały zalane ciekłą wodą około 620 milionów lat temu i że zostały wyrzucone z Marsa około 10,75 miliona lat temu w wyniku uderzenia asteroidy. Spadli na Ziemię w ciągu ostatnich 10 000 lat."""
 
-text_chassignite = """**Chassignity** to rzadki rodzaj meteorytu marsjańskiego, znany ze swojej unikalnej mineralogii."""
+text_chassignite = """**Chassignity** to rzadki rodzaj meteorytu marsjańskiego, znany ze swojej unikalnej mineralogii. Pierwszy meteoryt spadł w Chquirey w Haute-Marne we Francji w 1815 r. Znaleziono tylko jeden inny meteoryt, nazwany Northwest Africa (NWA) 2737. NWA 2737 został znaleziony w Maroku w sierpniu 2000 r. przez łowców meteorytów Bruno Fectaya i Carine Bidaut, która nadała mu tymczasową nazwę „Diderot”. Beck wykazał, że jego „mineralogia, chemia pierwiastków głównych i śladowych, a także izotopy tlenu ujawniły jednoznaczne marsjańskie pochodzenie i silne pokrewieństwo z Ch przypisanym”."""
 
-text_lunar = """**Meteoryty księżycowe** to fragmenty Księżyca, dostarczające cennych danych o geologii naszego naturalnego satelity."""
+text_lunar = """**Meteoryty księżycowe** to fragmenty Księżyca, dostarczające cennych danych o geologii naszego naturalnego satelity.Większość meteorytów księżycowych jest wyrzucana z Księżyca w wyniku uderzeń, w wyniku których powstają kratery księżycowe o średnicy kilku kilometrów lub mniejszej. Żaden krater źródłowy meteorytów księżycowych nie został zidentyfikowany, chociaż spekuluje się, że wysoce anomalny meteoryt księżycowy Sayh al Uhaymir 169 pochodzi z Krater uderzeniowy Lalande po prawej stronie Księżyca.
 
-text_aubrites = """**Aubryty** to grupa meteoryt6ów skalnych, charakteryzująca się wysoką zawartością minerałów piroksenowych."""
+Historia narażenia na promieniowanie kosmiczne ustalona na podstawie pomiarów gazów szlachetnych wykazała, że wszystkie meteoryty księżycowe zostały wyrzucone z Księżyca w ciągu ostatnich 20 milionów lat. Większość opuściła Księżyc w ciągu ostatnich 100 000 lat. Po opuszczeniu Księżyca większość meteoroidów księżycowych wchodzi na orbitę wokół Ziemi i ostatecznie ulega ziemskiej grawitacji. Niektóre meteoroidy wyrzucone z Księżyca są wystrzeliwane na orbity wokół Słońca. Te meteoroidy pozostają w kosmosie dłużej, ale ostatecznie przecinają orbitę Ziemi i ląd."""
 
-text_acapulcoite = """**Akapulkoity** to grupa meteorytów kamienno-żelaznych, zawierających zarówno metal jak i minerały skalne."""
+text_aubrites = """**Aubryty** to grupa meteorytów nazwana na cześć Aubresa, małego meteorytu achondrytowego, który spadł w pobliżu Nyons we Francji w 1836 roku. Składają się głównie z enstatytu ortopiroksenowego i często nazywane są achondrytami enstatytowymi. Ich magmowe pochodzenie odróżnia je od prymitywnych achondrytów enstatytowych i oznacza, że pochodzą z asteroidy.
 
-text_lodranite = """**Lodranity** to meteoryty kamienno-żelazne, różniące się od innych składem chemicznym."""
+Aubryty są zazwyczaj jasne i mają brązowawą skorupę. Często mówi się, że mają pochodzenie „księżycowe”."""
+
+text_acapulcoite = """**Akapulkoity** to grupa meteorytów kamienno-żelaznych, zawierających zarówno metal jak i minerały skalne. Zostały nazwane na cześć jedynego okazu w grupie, którego upadek był świadkiem. Meteoryt Acapulca spadł 11 sierpnia 1976 o godzinie 11:00 w pobliżu kolonii El Quemado (16,883°N 99,9°W), niedaleko Acapulco, Guerrero w Meksyku. Miał masę 1914 gramów. Kamień wydobyto 15 minut później z krateru o głębokości 30 centymetrów (12 cali) i był chłodny w dotyku. Po tym odkryciu 52 okazy meteorytów sklasyfikowano jako akapulkoity."""
+
+text_lodranite = """**Lodranity** to meteoryty kamienno-żelazne, różniące się od innych składem chemicznym. Nazwa grupy lodranitu pochodzi od Lodhran w Pakistanie, gdzie okaz typu spadł 1 października 1868 roku o godzinie 14:00. Naoczni świadkowie upadku zgłosili głośny huk, któremu towarzyszyła unosząca się chmura pyłu na wschód od miasta, co doprowadziło do odkrycia i odzyskania meteorytu. „Meteoryt z Lodranu” został po raz pierwszy opisany przez Gustava Tschermaka w 1870 roku. Opisał meteoryt jako „oprócz niklu i żelaza jest to agregaty oliwinowo-brązzytowe tak wyjątkowego rodzaju, że nigdy wcześniej nie znaleziono ich w meteorycie, jedynie przypominających ziemską skałę oliwinową”. George Thurland Prior jako pierwszy zaklasyfikował meteoryt lodran jako jedyny należący do grupy lodranitów. Dostrzegł także bliskie podobieństwa do ureilitów."""
 
 text_ureilites = """**Ureility** to meteoryty kamienno-żelazne, charakteryzujące się wysoką zawartością minerałów oliwinowych."""
 
